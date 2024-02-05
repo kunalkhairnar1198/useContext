fb9d9448da2534582b53a89ce64109838f5adb6c
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2024-02-05 13:47:10 +0530

    using useContext hook send data without a props to the Specific component and consume the component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {  useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../store/auth-context';
 
 const Home = (props) => {
+  //this useContext hook can hold AuthContext Component object and acccess inside the Home Component 
+  //this contextApi can use specific component to send data and component and access the component actions without any props
+  let ctx = useContext(AuthContext)
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button>
+      <Button onClick={ctx.onLogout}>Logout</Button>
     </Card>
   );
 };
