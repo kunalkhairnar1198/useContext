f986ce61c6309f07bd7479fcd14aa73430a338bf
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2024-02-05 17:02:09 +0530

    This custom provider manages authentication-related state and functions, to thes share App component this is root component that inside the all compoent subtrees

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { AuthContexProvider } from './components/store/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    
+    <AuthContexProvider>
+        <App />
+    </AuthContexProvider>
+);
+
+//This code is using custome context provider auth-context.js
+// AuthContextProvider, to wrap the main App component. This custom provider manages authentication-related state and functions, making them available to the App component and its descendants. It establishes a context for handling authentication throughout the application.
