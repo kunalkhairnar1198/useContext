dbc2497c4479a97ec8f651430c056c880d206a68
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2024-02-05 13:33:26 +0530

    using useContext hook in contextApi for directly  share state for Navigation component

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,34 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../store/auth-context';
 
 const Navigation = (props) => {
+  //remove the consumer and use useContext hook it makes redable and understandable code provide
+  const ctx = useContext(AuthContext);
+
   return (
-  <AuthContext.Consumer>
-    {(ctx)=>{
-      return  <nav className={classes.nav}>
-                <ul>
-                  {ctx.isLoggedIn && (
-                    <li>
-                      <a href="/">Users</a>
-                    </li>
-                  )}
-                  {ctx.isLoggedIn && (
-                    <li>
-                      <a href="/">Admin</a>
-                    </li>
-                  )}
-                  {ctx.isLoggedIn && (
-                    <li>
-                      <button onClick={props.onLogout}>Logout</button>
-                    </li>
-                  )}
-                </ul>
-              </nav>
-    }}
- 
-    </AuthContext.Consumer>
+   <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
